364657319729fb8e0cc3940ca7e4c49e1e75e481
Bárbaro Javier Valmaseda <bjvalmaseda.g@gmail.com>
2022-09-20 06:51:35 -0400

    :construction: [ADD] API connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Container } from "@mui/material";
+
+import Searcher from "./components/searcher";
+
+import { getGitHubUsers } from "./services/users";
 
 const App = () => {
-  return <div>Soy una platzinauta</div>;
+  const [inputUser, setInputUser] = useState("octocat");
+  const [data, setData] = useState({});
+
+  useEffect(async () => {
+    (async () => {
+      try {
+        const userResponse = await getGitHubUsers(inputUser);
+
+        if (inputUser === "octocat") {
+          localStorage.setItem("octocat", JSON.stringify(userResponse));
+        }
+
+        if (userResponse.message === "Not Found") {
+          const octocat = JSON.parse(localStorage.getItem("octocat"));
+          return setData(octocat);
+        }
+
+        setData(userResponse);
+      } catch (e) {
+        console.error(e.message);
+      }
+    })();
+  }, [inputUser]);
+
+  console.log(data);
+  return (
+    <Container
+      sx={{
+        background: "whitesmoke",
+        width: "80vw",
+        height: "500px",
+        borderRadius: "16px",
+        marginTop: "40px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <Searcher inputUser={inputUser} setInputUser={setInputUser} />
+    </Container>
+  );
 };
 
 export default App;
---
+++ src/services/users.js
@@ -0,0 +1,9 @@
+import { urlFetch } from "../constants";
+
+export const getGitHubUsers = async (user) => {
+  const response = await fetch(`${urlFetch}${user}`, {
+    method: "GET",
+  });
+  const payload = response.json();
+  return payload;
+};
